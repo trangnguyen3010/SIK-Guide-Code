5a103b3764bd2f49e8cef93c8af0068406ef000b
Trang Nguyen <nlptrang2001@gmail.com>
2024-05-29 16:25:31 +0700

    show the number of button click on the LCD

--- SIK_Circuit_4A-LCDHelloWorld/SIK_Circuit_4A-LCDHelloWorld.ino
+++ SIK_Circuit_4A-LCDHelloWorld/SIK_Circuit_4A-LCDHelloWorld.ino
@@ -15,18 +15,33 @@
 #include <LiquidCrystal.h>          //the liquid crystal library contains commands for printing to the display
 
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);   // tell the RedBoard what pins are connected to the display
+int buttonPin = 3;
+int buttonCount = 0;
 
 void setup() {
+  //set the button pin as inputs
+  pinMode(buttonPin, INPUT_PULLUP);
 
   lcd.begin(16, 2);                 //tell the lcd library that we are using a display that is 16 characters wide and 2 characters high
   lcd.clear();                      //clear the display
 }
 
 void loop() {
-
+  buttonCheck();
+  
   lcd.setCursor(0, 0);              //set the cursor to the 0,0 position (top left corner)
   lcd.print("Hello, world!");       //print hello, world! starting at that position
 
   lcd.setCursor(0, 1);              //move the cursor to the first space of the bottom row
-  lcd.print(millis() / 1000);       //print the number of seconds that have passed since the last reset
+  lcd.print(buttonCount);       //print the number of seconds that have passed since the last reset
+}
+
+void buttonCheck() {
+  if (digitalRead(buttonPin) == LOW) {
+    buttonCount += 1;
+    while (digitalRead(buttonPin) == LOW) {
+      // do nothing
+      // wait until the button is released
+    }
+  }
 }
