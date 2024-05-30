0393517814dced90f40db18d678c7e91e6e2b526
Trang Nguyen <nlptrang2001@gmail.com>
2024-05-30 15:16:27 +0700

    show temp in Celcius and Kelvin instead of Celcius and Farenheit

--- SIK_Circuit_4B-TemperatureSensor/SIK_Circuit_4B-TemperatureSensor.ino
+++ SIK_Circuit_4B-TemperatureSensor/SIK_Circuit_4B-TemperatureSensor.ino
@@ -16,7 +16,7 @@ LiquidCrystal lcd(13, 12, 11, 10, 9, 8);    // tell the RedBoard what pins are c
 
 float voltage = 0;                          //the voltage measured from the TMP36
 float degreesC = 0;                         //the temperature in Celsius, calculated from the voltage
-float degreesF = 0;                         //the temperature in Fahrenheit, calculated from the voltage
+float degreesK = 0;                         //the temperature in Kelvin, calculated from the voltage
 
 void setup() {
 
@@ -28,8 +28,8 @@ void loop() {
 
   voltage = analogRead(A0) * 0.004882813;   //convert the analog reading, which varies from 0 to 1023, back to a voltage value from 0-5 volts
   degreesC = (voltage - 0.5) * 100.0;       //convert the voltage to a temperature in degrees Celsius
-  degreesF = degreesC * (9.0 / 5.0) + 32.0; //convert the voltage to a temperature in degrees Fahrenheit
-
+  degreesK = degreesC + 273.15;             //convert the voltage to a temperature in degrees Kelvin
+  
   lcd.clear();                              //clear the LCD
 
   lcd.setCursor(0, 0);                      //set the cursor to the top left position
@@ -37,8 +37,8 @@ void loop() {
   lcd.print(degreesC);                      //print the degrees Celsius
 
   lcd.setCursor(0, 1);                      //set the cursor to the lower left position
-  lcd.print("Degrees F: ");                 //Print a label for the data
-  lcd.print(degreesF);                      //print the degrees Fahrenheit
+  lcd.print("Degrees K: ");                 //Print a label for the data
+  lcd.print(degreesK);                      //print the degrees Kelvin
 
   delay(1000);                              //delay for 1 second between each reading (this makes the display less noisy)
 }
