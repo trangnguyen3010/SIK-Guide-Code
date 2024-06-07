a017e9b888140d84e908d66ed627013f3f4220ef
trangnguyen3010 <66387098+trangnguyen3010@users.noreply.github.com>
2024-06-07 10:55:25 -0600

    make delay time between on and off state of LED is 14ms which makes the blinking invisible undetectable

--- SIK_Circuit_1A-Blink/SIK_Circuit_1A-Blink.ino
+++ SIK_Circuit_1A-Blink/SIK_Circuit_1A-Blink.ino
@@ -22,11 +22,10 @@ void loop() {
 
   digitalWrite(13, HIGH);   // Turn on the LED
 
-  delay(2000);              // Wait for two seconds
+  delay(14);              // Wait for two seconds
 
   digitalWrite(13, LOW);    // Turn off the LED
 
-  delay(2000);              // Wait for two seconds
-
+  delay(14);              // Wait for two seconds
 }
 
